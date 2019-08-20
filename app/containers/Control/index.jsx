fba285b490047e36db608984344d458513a3a2c2
伍开浩 <15310630063>
2019-08-20 22:19:46 +0800

    add echarts

--- app/containers/Control/index.jsx
+++ app/containers/Control/index.jsx
@@ -3,6 +3,7 @@ import Menu from "../../components/Control/Menu";
 import ControlHeader from "../../components/Control/ControlHeader";
 import TimeChoice from "../../components/Control/TimeChoice";
 import Panel from "../../components/Control/Panel";
+import ReactEcharts from 'echarts-for-react';
 
 const menuArr = [{
   key: 'home',
@@ -87,6 +88,55 @@ const panelArr = [{
 },
 ]
 
+const option = {
+  title: {
+      text: '折线图堆叠',
+      left: "center"
+  },
+  tooltip: {
+      trigger: 'axis'
+  },
+  legend: {
+      data:['处理数据条数','应用启动次数','应用使用人数'],
+      left: "85px"
+  },
+  grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+  },
+  xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一','周二','周三','周四','周五','周六','周日']
+  },
+  yAxis: {
+      type: 'value',
+  },
+  series: [
+      {
+          name:'处理数据条数',
+          type:'line',
+          stack: '总量',
+          data:[5, 6, 8, 9, 6, 7, 8]
+      },
+      {
+          name:'应用启动次数',
+          type:'line',
+          stack: '总量',
+          data:[2, 3, 4, 6, 5, 4, 3]
+      },
+      {
+          name:'应用使用人数',
+          type:'line',
+          stack: '总量',
+          data:[2.6, 3, 3.3, 3, 2.9, 2.2, 2]
+      },
+  ]
+};
+
+
 class Control extends Component {
   constructor(props) {
     super(props);
@@ -99,6 +149,9 @@ class Control extends Component {
       currentMenu: key
     })
   }
+  componentDidMount() {
+    
+  }
   render() {
     return (
       <div className="control">
@@ -107,7 +160,6 @@ class Control extends Component {
         </div>
         <div className="control-right">
           <ControlHeader />
-          <TimeChoice />
           <div className="panel-container">
             {
               panelArr.map((item, index) => {
@@ -117,6 +169,14 @@ class Control extends Component {
               })
             }
           </div>
+          <div className="divider"></div>
+          <TimeChoice />
+          <ReactEcharts
+            option={option}
+            notMerge={true}
+            lazyUpdate={true}
+            className="echarts"
+          />
         </div>
       </div>
     );
